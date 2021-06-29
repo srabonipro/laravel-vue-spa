e4cd8deae6d4b839245c9729e6f2cc9f23ade5c1
sraboni303 <sraboni391@gmail.com>
2021-06-29 11:59:34 +0600

    vuex setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
-import Vue from 'vue';
-import routes from './router/index';
 require('./bootstrap');
+import Vue from 'vue';
+import Vuex from 'vuex'
+import routes from './router/index';
 import { HasError, AlertError } from 'vform'
 import 'cxlt-vue2-toastr/dist/css/cxlt-vue2-toastr.css'
 import CxltToastr from 'cxlt-vue2-toastr'
+
+Vue.use(Vuex)
 
 var toastrConfigs = {
     position: 'top right',
